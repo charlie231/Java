a25241a204d647d2c5bc516eebe8717785b7f3f0
charlie231 <52510754+charlie231@users.noreply.github.com>
2023-04-02 19:30:20 +0900

    CeaserCipher2

--- Practice/CeaserCipher.java
+++ Practice/CeaserCipher.java
@@ -4,14 +4,23 @@ public class CeaserCipher {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     // 문자 입력받음
+    System.out.println("평문 입력");
     String a = sc.nextLine();
-    // 3만큼 이동
-    int shift = 3;
+
+    System.out.println("옮길 숫자 입력");
+    // 입력받은만큼 이동
+    int shift = sc.nextInt();
+
+    // 함수에 입력
     String encryptedText = encrypt(a, shift);
+    String deEncryptedText = deEncrypt(encryptedText, shift);
+
     System.out.println("Encrypted text: " + encryptedText);
+    System.out.println("DeEncrypted text: " + deEncryptedText);
   }
 
 
+  // 암호화 함수
   public static String encrypt(String plainText, int shift) {
     StringBuilder encryptedText = new StringBuilder();
 
@@ -19,6 +28,7 @@ public class CeaserCipher {
       // 문자열을 문자로 변환
       char ch = plainText.charAt(i);
       int num;
+
       // 문자가 맞을시
       if (Character.isLetter(ch)) {
         num = (int) ch;
@@ -36,11 +46,43 @@ public class CeaserCipher {
 
       // 문자가 아닐시
       else {
-        encryptedText.append(ch + "는 알파벳이 아닙니다.\n");
+        encryptedText.append(ch);
+      }
+    }
+    return encryptedText.toString();
+  }
+
+  // 복호화
+  public static String deEncrypt(String deencryptedText, int shift) {
+    StringBuilder deEncryptedText = new StringBuilder();
+
+    for (int i = 0; i < deencryptedText.length(); i++) {
+      // 문자열을 문자로 변환
+      char ch = deencryptedText.charAt(i);
+      int num;
+
+      // 문자가 맞을시
+      if (Character.isLetter(ch)) {
+        num = (int) ch;
+        // 대문자
+        if (num < 90) {
+          char shifted = (char) ('A' + (ch - 'A' - shift) % 26);
+          deEncryptedText.append(shifted);
+        }
+        // 소문자
+        else if (num >= 97 || num < 123) {
+          char shifted = (char) ('a' + (ch - 'a' - shift) % 26);
+          deEncryptedText.append(shifted);
+        }
+      }
+
+      // 문자가 아닐시
+      else {
+        deEncryptedText.append(ch);
       }
     }
 
-    return encryptedText.toString();
+    return deEncryptedText.toString();
   }
 
 
