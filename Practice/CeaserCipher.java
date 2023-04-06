f5c753c9922faae9a361f91611a42abd19404d51
charlie231 <52510754+charlie231@users.noreply.github.com>
2023-04-06 19:33:55 +0900

    Ceaser Cipher3

--- Practice/CeaserCipher.java
+++ Practice/CeaserCipher.java
@@ -1,23 +1,61 @@
 import java.util.Scanner;
 
 public class CeaserCipher {
+
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    // 문자 입력받음
-    System.out.println("평문 입력");
-    String a = sc.nextLine();
 
-    System.out.println("옮길 숫자 입력");
-    // 입력받은만큼 이동
-    int shift = sc.nextInt();
+    System.out.println("암호 복호화 시스템");
+    System.out.println("1: 시작");
+    System.out.println("2: 종료");
 
-    // 함수에 입력
-    String encryptedText = encrypt(a, shift);
-    String deEncryptedText = deEncrypt(encryptedText, shift);
+    // while문
+    int t = sc.nextInt();
 
-    System.out.println("Encrypted text: " + encryptedText);
-    System.out.println("DeEncrypted text: " + deEncryptedText);
+    while (t < 2) {
+      if (t == 1) {
+        System.out.println("모드 입력");
+        System.out.println("1: 암호문 생성");
+        System.out.println("2: 암호문 생성");
+        System.out.println("3: 취소");
+
+        int command = sc.nextInt();
+
+        switch (command) {
+          // 암호화
+          case 1: {
+            System.out.println("평문 입력");
+            String a1 = sc.nextLine(); // 두번입력 무시
+            String a = sc.nextLine();
+            System.out.println("움직일 숫자 입력");
+            int shift = sc.nextInt();
+            String EncryptedText = encrypt(a, shift);
+            System.out.println("Encrypted text: " + EncryptedText);
+            break;
+          }
+
+          // 복호화
+          case 2: {
+            System.out.println("암호문 입력");
+            String b = sc.nextLine();
+            System.out.println("움직일 숫자 입력");
+            int shift = sc.nextInt();
+            String deencryptedText = deEncrypt(b, shift);
+            System.out.println("DeEncrypted text: " + deencryptedText);
+            break;
+          }
+
+          case 3: {
+            break;
+          }
+        }
+      }
+    }
+
+
+
   }
+
 
 
   // 암호화 함수
@@ -52,6 +90,7 @@ public class CeaserCipher {
     return encryptedText.toString();
   }
 
+
   // 복호화
   public static String deEncrypt(String deencryptedText, int shift) {
     StringBuilder deEncryptedText = new StringBuilder();
@@ -84,7 +123,6 @@ public class CeaserCipher {
 
     return deEncryptedText.toString();
   }
-
-
 }
+
 
